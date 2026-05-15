e120838baa0a302c8673f4c2b0ca73acbe431f02
Shajini-A <2023it0009@svce.ac.in>
2026-05-15 18:26:27 +0530

    Fix: Enable static serving for uploads (View Attachment fix)

--- server/index.js
+++ server/index.js
@@ -13,8 +13,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Debug logger
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
